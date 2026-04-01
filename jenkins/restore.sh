a1b22209b519543625e69924c0a626a9696f2d54
AnhMaiTuan <amaituan@openwt.com>
2026-04-01 21:19:20 +0700

    fix: Update backup data

--- jenkins/restore.sh
+++ jenkins/restore.sh
@@ -7,3 +7,6 @@ MSYS_NO_PATHCONV=1 docker run --rm \
   -v "$(pwd):/jenkins" \
   busybox:1.37.0 \
   sh -c "tar xzf /jenkins/jenkins-backup.tar.gz -C /data --strip-components=1"
+
+# docker compose -f docker-compose.backups.yml build --no-cache --progress=plain 2>&1
+# docker compose -f docker-compose.backups.yml up -d
